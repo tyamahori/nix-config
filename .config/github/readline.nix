374b41ccf96334a5138deb48210f5cfd415f16ca
tyamahori <tyamahori@gmail.com>
2025-01-13 11:42:32 +0900

    add readline nix

---
+++ .config/github/readline.nix
@@ -0,0 +1,27 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.stdenv.mkDerivation {
+  pname = "readline";
+  version = "8.2";
+
+  src = pkgs.fetchurl {
+    url = "http://ftp.gnu.org/pub/gnu/readline/readline-8.2.tar.gz";
+    hash = "sha256-P+txcfFqhO6CyhijbXub4QmlLAT0kqBTMx19EJUAfDU=";
+  };
+
+  # ビルドに必要な依;存パッケージ
+  buildInputs = [ pkgs.gnumake ];
+
+  # ビルドコマンド
+  buildPhase = ''
+    ls -la
+    ./configure
+    ${pkgs.gnumake}/bin/make
+  '';
+
+  # インストールフェーズ
+  installPhase = ''
+    mkdir -p $out/bin
+    cp readline $out/bin/
+  '';
+}
